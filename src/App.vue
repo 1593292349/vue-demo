a58f69b7adc06aa87546c47df1943f54ec297546
唐国雄 <1593292349@qq.com>
2023-12-15 16:53:05 +0800

    test: 测试例子

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
 	<div>
 		{{createVue()}}
+		<button @click="++count">render</button>
+		{{count}}
 	</div>
 </template>
 
 <script setup lang="ts">
 import Vue,{
+	shallowRef,
 	getCurrentScope,
 } from 'vue';
 
 window.getCurrentScope=getCurrentScope;
 
+const count=shallowRef(1);
 let vm:Vue;
 function createVue(){
 	console.log('createVue',vm);
@@ -20,6 +24,8 @@ function createVue(){
 				return h('div');
 			}
 		}).$mount();
+	}else{
+		vm.$forceUpdate();
 	}
 }
 </script>
